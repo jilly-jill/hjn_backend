df03c5a0b4caaa1cbec910c994b12a9c593ad0b6
Jillian Sechrest <93422213+jilly-jill@users.noreply.github.com>
2022-05-13 04:13:57 -0400

    Add files via upload

---
+++ hjnws/src/main/java/com/webapp/hjnws/HjnwsApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.hjnws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjnwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjnwsApplication.class, args);
+	}
+
+}
